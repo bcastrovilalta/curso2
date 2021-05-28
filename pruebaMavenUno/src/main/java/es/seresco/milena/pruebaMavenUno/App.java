f0c3e5bc1bbffdbe3eca82c4b786f8d32a61378d
Bernardo <bernardo.castro@seresco.es>
2021-05-28 13:19:51 +0200

    Versión inicial

---
+++ pruebaMavenUno/src/main/java/es/seresco/milena/pruebaMavenUno/App.java
@@ -0,0 +1,33 @@
+package es.seresco.milena.pruebaMavenUno;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger logger = LogManager.getLogger(App.class);
+	
+	public static String devuelvePatata()
+	{
+		return("Patata");
+	}
+	
+	public static String devuelvePiloro()
+	{
+		return("Piloro");
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+                
+        logger.info("Traza desde logger");
+        
+        logger.info("Patata devuelve {}", devuelvePatata());
+        
+    }
+}
---
+++ pruebaMavenUno/src/test/java/es/seresco/milena/pruebaMavenUno/AppTest.java
@@ -0,0 +1,28 @@
+package es.seresco.milena.pruebaMavenUno;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    
+    @Test
+    public void testDevuelvePatata()
+    {    	
+    	//assertTrue(App.devuelvePatata().toUpperCase().equals("PATATA")); 
+    	assertEquals("PATATA", App.devuelvePatata().toUpperCase());    	
+    }
+    
+}
